dc3050c6b33ab2ad1fc75c5b9cfc535fdfeea722
DanilaMaher <daniilnews@gmail.com>
2018-11-26 14:43:08 +0300

    TracedMethod class base functions implemented

--- Tracer/TracedMethod.cs
+++ Tracer/TracedMethod.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,22 +11,36 @@ namespace Tracer
     {
         private String name;
         private String className;
-        private long workTime;
         private List<TracedMethod> nestedMethods;
+        private Stopwatch stopwatch;
+
+        public TracedMethod (String name, String className)
+        {
+            this.name = name;
+            this.className = className;
+            nestedMethods = new List<TracedMethod>();
+            stopwatch = new Stopwatch();
+        }
+
+        public long GetWorkTime()
+        {
+            return stopwatch.ElapsedMilliseconds;
+        }
 
         public void StartTrace()
         {
-
+            stopwatch.Start();
+            
         }
 
         public void StopTrace()
         {
-
+            stopwatch.Stop();
         }
 
         public void AddNestedMethod (TracedMethod method)
         {
-
+            nestedMethods.Add(method);
         }
 
     }
