9b3dd6e9a573b32cbd60892fd0a4f6c097d606c2
DanilaMaher <daniilnews@gmail.com>
2018-11-26 15:24:25 +0300

    TracedThread class base functions implemented

--- Tracer/TracedThread.cs
+++ Tracer/TracedThread.cs
@@ -8,5 +8,28 @@ namespace Tracer
 {
     class TracedThread
     {
+        private List<TracedMethod> methods;
+        private Stack<TracedMethod> methodsCallStack;
+
+        public TracedThread()
+        {
+            methods = new List<TracedMethod>();
+            methodsCallStack = new Stack<TracedMethod>();
+        }
+
+        public void StartTraceMethod(TracedMethod method)
+        {
+            if (methodsCallStack.Count == 0)
+            {
+                methods.Add(method);
+            }
+            methodsCallStack.Push(method);
+            method.StartTrace();
+        }
+
+        public void StopTraceMethod()
+        {
+            methodsCallStack.Pop().StopTrace();
+        }
     }
 }
